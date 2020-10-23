03ecb807f74313e1b563ec5369c98f8d980fdb9f
ddeexxiikk <jakub.batycki.jr@gmail.com>
2020-10-23 22:40:17 +0200

    Update main.cpp

--- PP0501A - NWD/main.cpp
+++ PP0501A - NWD/main.cpp
@@ -4,25 +4,21 @@ using namespace std;
 
 int nwd(int a, int b){
     int wynik, mniejsza;
-
     if(a>b){
         mniejsza=b;
     }else{
         mniejsza=a;
     }
-
     for(int i=1; i<=mniejsza; i++){
         if(a%i==0 && b%i==0){
             wynik=i;
         }
     }
-
     return wynik;
 }
 
 int main()
 {
-
     int ilosc, pierwsza, druga;
     cin >> ilosc;
     for(int i=0; i<ilosc; i++){
